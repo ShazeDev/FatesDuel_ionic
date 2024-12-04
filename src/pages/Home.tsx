5563420faa845ad2e673fc7fc7c784ad05a193ab
Tarde <shazedeveloper@gmail.com>
2024-12-04 16:56:15 +0000

    Quitado video

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,6 +1,6 @@
-import React, {useEffect} from 'react';
-import { IonPage, IonContent} from '@ionic/react';
-import { useHistory } from 'react-router-dom'; 
+import React, { useEffect } from 'react';
+import { IonPage, IonContent } from '@ionic/react';
+import { useHistory } from 'react-router-dom';
 import './Home.css';
 import CustomButton from '../components/CustomButton';
 import { useMusic } from '../MusicContext';
@@ -8,7 +8,7 @@ import { useMusic } from '../MusicContext';
 
 const Home: React.FC = () => {
   const history = useHistory();
-  const { playMusic, setMusicSource } = useMusic(); 
+  const { playMusic, setMusicSource } = useMusic();
 
   useEffect(() => {
     setMusicSource('/sounds/TranquilFieldsEastern.mp3');  // Establece la fuente de la música
@@ -19,25 +19,20 @@ const Home: React.FC = () => {
   const handleNewGame = () => {
     history.push('/characterSelect'); // Navegación programática
   };
-  
+
   return (
     <IonPage>
       <IonContent fullscreen>
         {/* Contenedor principal */}
         <div className="home-container">
-          {/* Video de fondo */}
-          <video autoPlay loop muted className="home-video">
-            <source src="/videos/menuVideo.mp4" type="video/mp4" />
-            Tu navegador no soporta el elemento de video.
-          </video>
 
           {/* Contenido del menú */}
           <div className="home-content">
             <h1 className="game-title">Fate's Duel</h1>
             <div>
               {/* Botón de navegación usando routerLink */}
-              <CustomButton label="Nueva Partida" onClick={handleNewGame} sound="/sounds/MENU_Pick.wav"/>
-            
+              <CustomButton label="Nueva Partida" onClick={handleNewGame} sound="/sounds/MENU_Pick.wav" />
+
 
             </div>
           </div>
